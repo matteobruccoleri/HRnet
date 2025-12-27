3f45544dc7aab18e83c2c17f62d7f0552074df8b
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2025-12-27 18:31:11 +0100

    feat: delete file;

--- src/index.js
+++
@@ -1,3 +0,0 @@
-// Point d'entrée de la bibliothèque : expose le composant SmartModal.
-export { default as SmartModal } from "./components/SmartModal.jsx";
-export { default } from "./components/SmartModal.jsx";
